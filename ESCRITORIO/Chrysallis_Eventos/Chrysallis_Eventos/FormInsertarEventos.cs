5cbf34f1c55f35b90fb97c194baff5f6b6567404
Daniel Martos Arroyo <dmartosarroyo@gmail.com>
2021-03-25 14:47:02 +0100

    Actualizado

--- ESCRITORIO/Chrysallis_Eventos/Chrysallis_Eventos/FormInsertarEventos.cs
+++ ESCRITORIO/Chrysallis_Eventos/Chrysallis_Eventos/FormInsertarEventos.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Chrysallis_Eventos.MODELOS;
 
@@ -47,13 +41,9 @@ namespace Chrysallis_Eventos
                 dateTimePickerEvento.Value = DateTime.Now;
                 comboBoxTipoEvento.SelectedItem = null;
                 comboBoxProvincias.SelectedItem = null;
-
-
-
             }
             else
             {
-
                 textBoxTituloEvento.Text = esdeveniment.titol;
                 dateTimePickerEvento.Value = esdeveniment.data;
                 comboBoxTipoEvento.SelectedItem = esdeveniment.tipus_events;
@@ -67,7 +57,6 @@ namespace Chrysallis_Eventos
                 textBoxMinimoAsistentesEvento.Text = esdeveniment.quantitat_mínima.ToString();
                 textBoxMaximoasistentesEvento.Text = esdeveniment.quantitat_max.ToString();
                 textBoxTotalparticipantesEvento.Text = esdeveniment.cont_assitents.ToString();
-
             }
         }
 
@@ -110,36 +99,52 @@ namespace Chrysallis_Eventos
             Boolean correcto = false;
 
             if (textBoxTituloEvento.Text.Length == 0)
-            { 
-            
+            {              
+                MessageBox.Show("Debe introducir un título de evento", "Advertencia",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBoxTituloEvento.Focus();
             }
             else if (comboBoxTipoEvento.SelectedItem == null)
-            { 
-            
+            {
+                MessageBox.Show("Debe seleccionar un tipo de evento", "Advertencia",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                comboBoxTipoEvento.Focus();
             }
             else if (comboBoxProvincias.SelectedItem == null)
-            { 
-            
+            {
+                MessageBox.Show("Debe seleccionar una provincia", "Advertencia",
+                  MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                comboBoxProvincias.Focus();
             }
             else if (comboBoxCiudadesBuscadas.SelectedItem == null)
-            { 
-            
+            {
+                MessageBox.Show("Debe seleccionar una ciudad/población", "Advertencia",
+                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                comboBoxCiudadesBuscadas.Focus();
             }
             else if (textBoxDireccionEvento.Text.Length == 0)
-            { 
-            
+            {
+                MessageBox.Show("Debe introducir una dirección de evento", "Advertencia",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBoxDireccionEvento.Focus();
             }
             else if (richTextBoxDescripcionEvento.Text.Length == 0)
-            { 
-            
+            {
+                MessageBox.Show("Debe introducir una descripción de evento", "Advertencia",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                richTextBoxDescripcionEvento.Focus();
             }
             else if (textBoxMinimoAsistentesEvento.Text.Length == 0)
             {
-
+                MessageBox.Show("Debe introducir un número mínimo de asistentes", "Advertencia",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBoxMinimoAsistentesEvento.Focus();
             }
             else if (textBoxMaximoasistentesEvento.Text.Length == 0)
             {
-
+                MessageBox.Show("Debe introducir un número máximo de asistentes", "Advertencia",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBoxMaximoasistentesEvento.Focus();
             }
             else
             {
@@ -147,7 +152,9 @@ namespace Chrysallis_Eventos
                 {
                     if(textBoxPrecioPersonaEvento.Text.Length == 0)
                     {
-
+                        MessageBox.Show("Debe introducir un precio por persona", "Advertencia",
+                            MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        textBoxPrecioPersonaEvento.Focus();
                     }
                     else
                     {
@@ -155,7 +162,9 @@ namespace Chrysallis_Eventos
                         {
                             if(textBoxTotalparticipantesEvento.Text.Length == 0)
                             {
-
+                                MessageBox.Show("Debe introducir el número de participantes", "Advertencia",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                                textBoxTotalparticipantesEvento.Focus();
                             }
                             else
                             {
@@ -175,7 +184,9 @@ namespace Chrysallis_Eventos
                     {
                         if (textBoxTotalparticipantesEvento.Text.Length == 0)
                         {
-
+                            MessageBox.Show("Debe introducir el número de participantes", "Advertencia",
+                                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            textBoxTotalparticipantesEvento.Focus();
                         }
                         else
                         {
