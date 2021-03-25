a829c9b803d35ab50b28448666d274ef3839b107
Daniel Martos Arroyo <dmartosarroyo@gmail.com>
2021-03-25 10:23:34 +0100

    Actualizado

--- ESCRITORIO/Chrysallis_Eventos/Chrysallis_Eventos/Program.cs
+++ ESCRITORIO/Chrysallis_Eventos/Chrysallis_Eventos/Program.cs
@@ -16,7 +16,7 @@ namespace Chrysallis_Eventos
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormInicial());
+            Application.Run(new FormCrearAdmin());
         }
     }
 }
