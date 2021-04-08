9c2cbbc2860fa422bac271cf4eef133838b0b132
Oriol Miralles Peiron <omp@galenicum.com>
2021-04-08 10:05:51 +0200

    commit

--- ANDROID/Chrysallis/app/src/main/java/com/example/chrysallis/Api/Api.java
+++ ANDROID/Chrysallis/app/src/main/java/com/example/chrysallis/Api/Api.java
@@ -7,8 +7,8 @@ public class Api {
     private static final String BASE_URL = "http://10.0.2.2:55498/";
     private static Retrofit retrofit = null;
 
-    public static Retrofit getApi() {
-        if (retrofit == null) {
+    public static Retrofit getApi(){
+        if(retrofit==null){
             retrofit = new Retrofit
                     .Builder()
                     .baseUrl(BASE_URL)
