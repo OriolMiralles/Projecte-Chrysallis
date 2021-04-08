4653c2184e13e7a073d1f51dd6281fd508c97d44
Daniel Martos Arroyo <dmartosarroyo@gmail.com>
2021-04-08 13:30:16 +0200

    pull

--- ANDROID/Chrysallis/app/src/main/java/com/example/chrysallis/Api/Api.java
+++ ANDROID/Chrysallis/app/src/main/java/com/example/chrysallis/Api/Api.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Api {
-    private static final String BASE_URL = "http://10.0.2.2:55498/";
+    private static final String BASE_URL = "http://10.0.2.2:57336/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getApi(){
