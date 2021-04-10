0ae0e0052463b54ae7e9a1f4eaefdf430e963cc7
dmartosa33@cepnet.net <dmartosa33@cepnet.net>
2021-04-10 18:45:46 +0200

    pull

--- ANDROID/Chrysallis/app/src/main/java/com/example/chrysallis/EsdevenimentListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.chrysallis;
-
-import com.example.chrysallis.Models.Esdeveniment;
-
-public interface EsdevenimentListener {
-    public void onSelectedEsdeveniment(Esdeveniment esdeveniment);
-}
